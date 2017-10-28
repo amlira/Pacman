4009811f639a82d6df8a882d7fe612565b77759b
amlira <30601882+amlira@users.noreply.github.com>
2017-10-27 23:10:08 -0200

    Boca do Pac
    Abertura na elipse para reapresentar a boca do pacman.

--- pacman.js
+++ pacman.js
@@ -38,12 +38,13 @@ function draw() {
 	
 	//Gerar cenário
 	for(j = 0; j < cenario.length; j++) {
-	for(i = 0; i < cenario.length; i++) {
-		if(cenario[i][j] == "#") {
-			image(img, j*tamBloco, i*tamBloco);
+		for(i = 0; i < cenario.length; i++) {
+			if(cenario[i][j] == "#") {
+				image(img, j*tamBloco, i*tamBloco);
+			}
 		}
 	}
-}
+	
 	//movimentos do pacman
 	if (keyIsDown(LEFT_ARROW))
 	px-=4;
@@ -57,41 +58,20 @@ function draw() {
 	if (keyIsDown(DOWN_ARROW))
 	py+=4;
 
-	ellipse(px, py, 40, 40); //imagem do pacman
-
-	/* //inicio colisao com limite do espaço
-	//limite em x do espaço
-	if (px > 400) {
-		px = 400;
-	}
+	// ellipse(px, py, 40, 40); //imagem do pacman sem boca
+	arc(px, py, 40, 40, PI/6, 11*PI/6, PIE); //imagem do pacman com boca	  
 	
-	if (px < 0) {
-		px = 0;
-	}
-
-	//limite em y do espaço
-	if (py > 400) {
-		py = 400;
-	}
-	
-	if (py < 0) {
-		py = 0;
-	}
-	
-	//fim colisao */
-	  
-  
-	/*//movimentos quadrado
+/*	//movimentos quadrado
 	if (posX < 400){
 		posX = posX + 15;
 	}
 	else{
 		posX = 0;
 	} 
-	rect(posX, posY, 50, 50);*/
-	
-	// colisao não está dando certo
+	rect(posX, posY, 40, 40);*/
 
+	
+	// inicio de todas as colisões
 	function colisao () {
 		pColuna = Math.floor(px/tamBloco); 
 		pLinha = Math.floor(py/tamBloco);
@@ -104,28 +84,47 @@ function draw() {
 		}
 	}
 		
-		if (keyIsDown(LEFT_ARROW)) {
-			if (colisao (pColuna, pLinha)){
-				px=px+4
-			}
+	if (keyIsDown(LEFT_ARROW)) {
+		if (colisao (pColuna, pLinha)){
+			px=px+4
 		}
-		
-		if (keyIsDown (RIGHT_ARROW)) {
-			if (colisao (pColuna-1, pLinha-1)){
-				px=px-4
-			}
-		}
-		
-		if (keyIsDown (UP_ARROW)) {
-			if (colisao (pColuna, pLinha)){
-				py=py+4
-			}
-		}
-		
-		if (keyIsDown (DOWN_ARROW)) {
-			if (colisao (pColuna, pLinha)){
-				py=py-4
-			}
-		}
+	}
 	
+	if (keyIsDown (RIGHT_ARROW)) {
+		if (colisao (pColuna-1, pLinha-1)){
+			px=px-4
+		}
+	}
+	
+	if (keyIsDown (UP_ARROW)) {
+		if (colisao (pColuna, pLinha)){
+			py=py+4
+		}
+	}
+	
+	if (keyIsDown (DOWN_ARROW)) {
+		if (colisao (pColuna, pLinha)){
+			py=py-4
+		}
+	}
+	//inicio colisao com limite do espaço
+	//limite em x do espaço
+	if (px > 400) {
+		px = 0;
+	}
+	
+	if (px < 0) {
+		px = 400;
+	}
+
+	//limite em y do espaço. O pac não está voltando para o outro lado
+/*	if (py > 400) {
+		py = 0;
+	}
+	
+	if (py < 0) {
+		py = 400;
+	}*/
+	
+	//fim colisao
 }
