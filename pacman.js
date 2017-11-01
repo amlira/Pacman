7bd08207fd92e0f380ece75b4b59691e2c1b6eb4
amlira <30601882+amlira@users.noreply.github.com>
2017-11-01 21:05:26 -0200

    Tamanho da Matriz
    Ajuste do tamanho da matriz para caber na tela.

--- pacman.js
+++ pacman.js
@@ -1,13 +1,14 @@
-var px = 380; //x do pacman
-var py = 620; //y do pacman
+var px = 280; //x do pacman
+var py = 460; //y do pacman
 var posX, posY; //x e y do quadrado
 var pColuna;
 var pLinha;
-var tamBloco = 40;
+var tamBloco = 30;
 var img;
 var colisao = false;
-var canvasX = 780;
-var canvasY = 840;
+var naTela = true;
+var canvasX = 570;
+var canvasY = 630;
 
 cenario = [
 //		["1", "2", "3", "4", "5", "6", "7", "8", "9", "1", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
@@ -18,18 +19,18 @@ cenario = [
 /*5*/	["#", "v", "#", "#", "v", "#", "v", "#", "#", "#", "#", "#", "v", "#", "v", "#", "#", "v", "#"],
 /*6*/	["#", "v", "v", "v", "v", "#", "v", "v", "v", "#", "v", "v", "v", "#", "v", "v", "v", "v", "#"],
 /*7*/	["#", "#", "#", "#", "v", "#", "#", "#", "v", "#", "v", "#", "#", "#", "v", "#", "#", "#", "#"],
-/*8*/	["v", "v", "v", "#", "v", "#", "v", "v", "v", "v", "v", "v", "v", "#", "v", "#", "v", "v", "#"],
+/*8*/	["v", "v", "v", "#", "v", "#", "v", "v", "v", "v", "v", "v", "v", "#", "v", "#", "v", "v", "v"],
 /*9*/	["#", "#", "#", "#", "v", "#", "v", "#", "#", "#", "#", "#", "v", "#", "v", "#", "#", "#", "#"],
-/*10*/	["v", "v", "v", "v", "v", "v", "v", "#", "v", "v", "v", "#", "v", "v", "v", "v", "v", "v", "#"],
+/*10*/	["v", "v", "v", "v", "v", "v", "v", "#", "v", "v", "v", "#", "v", "v", "v", "v", "v", "v", "v"],
 /*11*/	["#", "#", "#", "#", "v", "#", "v", "#", "#", "#", "#", "#", "v", "#", "v", "#", "#", "#", "#"],
-/*12*/	["v", "v", "v", "#", "v", "#", "v", "v", "v", "v", "v", "v", "v", "#", "v", "#", "v", "v", "#"],
+/*12*/	["v", "v", "v", "#", "v", "#", "v", "v", "v", "v", "v", "v", "v", "#", "v", "#", "v", "v", "v"],
 /*13*/	["#", "#", "#", "#", "v", "#", "v", "#", "#", "#", "#", "#", "v", "#", "v", "#", "#", "#", "#"],
 /*14*/	["#", "v", "v", "v", "v", "v", "v", "v", "v", "#", "v", "v", "v", "v", "v", "v", "v", "v", "#"],
-/*15*/	["#", "v", "#", "#", "v", "#", "#", "#", "v", "#", "v", "#", "#", "#", "v", "#", "#", "#", "#"],
+/*15*/	["#", "v", "#", "#", "v", "#", "#", "#", "v", "#", "v", "#", "#", "#", "v", "#", "#", "v", "#"],
 /*16*/	["#", "v", "v", "#", "v", "v", "v", "v", "v", "v", "v", "v", "v", "v", "v", "#", "v", "v", "#"],
 /*17*/	["#", "#", "v", "#", "v", "#", "v", "#", "#", "#", "#", "#", "v", "#", "v", "#", "v", "v", "#"],
 /*18*/	["#", "v", "v", "v", "v", "#", "v", "v", "v", "#", "v", "v", "v", "#", "v", "v", "v", "v", "#"],
-/*19*/	["#", "v", "#", "#", "#", "#", "#", "#", "v", "#", "v", "#", "#", "#", "#", "#", "#", "#", "#"],
+/*19*/	["#", "v", "#", "#", "#", "#", "#", "#", "v", "#", "v", "#", "#", "#", "#", "#", "#", "v", "#"],
 /*20*/	["#", "v", "v", "v", "v", "v", "v", "v", "v", "v", "v", "v", "v", "v", "v", "v", "v", "v", "#"],
 /*21*/	["#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#"],
 	
@@ -38,19 +39,19 @@ cenario = [
 	
 // carregar imagem
 function preload() {
-  img = loadImage("img.png");
+  img = loadImage("img30p.png");
 }
  
 function setup() {
 	createCanvas(canvasX, canvasY);
-	posX = 0;
-	posY = 200;
+	posY = random(400); 
+	posX = 15;
 }
   
 function draw() { 
 
-	background(0);
-	
+	background(105,105,105);
+
 	//Gerar cenário
 	for(j = 0; j < cenario.length; j++) {
 		for(i = 0; i < cenario.length; i++) {
@@ -74,16 +75,25 @@ function draw() {
 	py+=4;
 
 	// ellipse(px, py, 40, 40); //imagem do pacman sem boca
-	arc(px, py, 40, 40, PI/6, 11*PI/6, PIE); //imagem do pacman com boca	  
+	arc(px, py, 30, 30, PI/6, 11*PI/6, PIE); //imagem do pacman com boca	 
+	fill (250,250,0);
 	
-/*	//movimentos quadrado
-	if (posX < 400){
-		posX = posX + 15;
+	//movimentos quadrado
+	if(naTela){
+		posX += 15;
 	}
 	else{
-		posX = 0;
-	} 
-	rect(posX, posY, 40, 40);*/
+		posY = random(400); 
+		posX = 15;
+		naTela = true;
+	}	
+	if (posX > canvasX){
+		posX = 0
+	}
+	if (posX < 0){
+		posX = canvasX;
+	}
+	rect(posX, posY, 40, 40);
 
 	
 	// inicio de todas as colisões
@@ -100,25 +110,25 @@ function draw() {
 	}
 		
 	if (keyIsDown(LEFT_ARROW)) {
-		if (colisao (pColuna, pLinha)){
+		if (colisao (pLinha, pColuna)){
 			px=px+4
 		}
 	}
 	
 	if (keyIsDown (RIGHT_ARROW)) {
-		if (colisao (pColuna-1, pLinha-1)){
+		if (colisao (pLinha-1, pColuna-1)){
 			px=px-4
 		}
 	}
 	
 	if (keyIsDown (UP_ARROW)) {
-		if (colisao (pColuna, pLinha)){
+		if (colisao (pLinha, pColuna)){
 			py=py+4
 		}
 	}
 	
 	if (keyIsDown (DOWN_ARROW)) {
-		if (colisao (pColuna, pLinha)){
+		if (colisao (pLinha, pColuna)){
 			py=py-4
 		}
 	}
@@ -133,12 +143,12 @@ function draw() {
 	}
 
 	//limite em y do espaço. O pac não está voltando para o outro lado
-/*	if (py > 400) {
+/*	if (py > canvasY) {
 		py = 0;
 	}
 	
 	if (py < 0) {
-		py = 400;
+		py = canvasY;
 	}*/
 	
 	//fim colisao
