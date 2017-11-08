d01ba6cfdc1cc9d6b9efa0dcd1cf264fcab60db5
amlira <30601882+amlira@users.noreply.github.com>
2017-11-08 17:21:52 -0200

    Pontuação
    Adicionado a pontuação
    Jogo para quando o pacman perde 3 vidas
    Tela de Game Over

--- pac.js
+++ pac.js
@@ -16,14 +16,6 @@ var corBranco = true;
 var vidas = 3;
 var pontos = 0;
 
-/*//variaveis para gerar varios objetos
-var vx = []; 
-var vy = [];
-var vdx = []
-var vdy = []
-var qt = 10;
-var vtam = 40;
-*/
 
 // carregar imagem
 function preload() {
@@ -65,6 +57,18 @@ function colisao (px, py) {
 		return false;
 	}
 }
+
+function colisaocomidinha (cx, cy) {
+	pColuna = Math.floor(cx/tamBloco); 
+	pLinha = Math.floor(cy/tamBloco);
+	
+	if (cenario[pLinha][pColuna] == "v") {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
 	
 function setup () {
 	createCanvas(canvasX, canvasY);
@@ -75,124 +79,116 @@ function setup () {
 	
 	dfx = 5 //random (-5, 5);
 	dfy = 0 //random (-5, 5);
-	
-/*	//gerar varios objetos
-	for ( i = 0; i < qt; i++) { 
-		vx[i] = random(0,canvasY); 
-		vy[i] = random(0,canvasY);
-		vdx[i] = random(-6,6);
-		vdy[i] = random(-6,6);
-	*/
 }
 	
 function draw () {
 	background (0);
-	
-	//Gerar cenário
-	for(j = 0; j < cenario.length; j++) {
-		for(i = 0; i < cenario.length; i++) {
-			if(cenario[i][j] == "#") {
-				//image(img, j*tamBloco, i*tamBloco);
-				fill(100,100,255);
-				rect(j*tamBloco,i*tamBloco,tamBloco,tamBloco);
+	if(vidas != 0) {	
+		//Gerar cenário
+		for(j = 0; j < cenario.length; j++) {
+			for(i = 0; i < cenario.length; i++) {
+				if(cenario[i][j] == "#") {
+					//image(img, j*tamBloco, i*tamBloco);
+					fill(100,100,255);
+					rect(j*tamBloco,i*tamBloco,tamBloco,tamBloco);
+				}
 			}
 		}
-	}
-	
-	//comidinhas
-	for(j = 0; j < cenario.length; j++) {
-		for(i = 0; i < cenario.length; i++) {
-			if(cenario[i][j] == "v") {
-				//image(img, j*tamBloco, i*tamBloco);
-				fill(255,255,255);
-				ellipse(j*tamBloco+tamBloco/2,i*tamBloco+tamBloco/2,10,10);
-				comidinha = true;
+		
+		//comidinhas
+		for(j = 0; j < cenario.length; j++) {
+			for(i = 0; i < cenario.length; i++) {
+				if(cenario[i][j] == "v") {
+					//image(img, j*tamBloco, i*tamBloco);
+					fill(255,255,255);
+					ellipse(j*tamBloco+tamBloco/2,i*tamBloco+tamBloco/2,10,10);
+					//comidinha = true;
+				}
+				
 			}
+		}
 			
-		}
-	}
+		//movimentos fantasma
+		fx = fx + dfx;
+		fy = fy + dfy;
 		
-	//movimentos fantasma
-	fx = fx + dfx;
-	fy = fy + dfy;
-	
-	//movimentos pacMan
-	if (keyIsDown(LEFT_ARROW)) {
-		if ( ! colisao (x - 4 - tamBloco/2, y)) {
-			x = x - 4;       
+		//movimentos pacMan
+		if (keyIsDown(LEFT_ARROW)) {
+			if ( ! colisao (x - 4 - tamBloco/2, y)) {
+				x = x - 4;       
+			}
+			if(colisaocomidinha) {
+				pontos++; 
+			}
 		}
-	}
-	
-	if (keyIsDown(RIGHT_ARROW)) {
-		if ( ! colisao (x + 4 + tamBloco/2, y)) {
-			x = x + 4;       
+		
+		if (keyIsDown(RIGHT_ARROW)) {
+			if ( ! colisao (x + 4 + tamBloco/2, y)) {
+				x = x + 4;       
+			}
+			if(colisaocomidinha) {
+				pontos++; 
+			}
 		}
-	}
-	
-	if (keyIsDown (UP_ARROW)) {
-		if ( ! colisao (x, y - 4 - tamBloco/2)){
-			y = y - 4;
+		
+		if (keyIsDown (UP_ARROW)) {
+			if ( ! colisao (x, y - 4 - tamBloco/2)){
+				y = y - 4;
+			}
+			if(colisaocomidinha) {
+				pontos++; 
+			}
 		}
-	}
-	
-	if (keyIsDown (DOWN_ARROW)) {
-		if ( ! colisao ( x, y + 4 + tamBloco/2)){
-			y = y + 4
+		
+		if (keyIsDown (DOWN_ARROW)) {
+			if ( ! colisao ( x, y + 4 + tamBloco/2)){
+				y = y + 4
+			}
+			if(colisaocomidinha) {
+				pontos++; 
+			}
 		}
-	}
-	
-	//relacao do fantasma com fim do espaço
-	if ( fx > 400) {
-		fx = 0;   
-	}
-	if ( fy > 400) {
-		fy = 0;   
-	}
-	if ( fx < 0 ) {
-		fx = 400;   
-	}
-	if ( fy < 0 ) {
-		fy = 400;  
-	}
-	
-	//colisão entre pacMan e fantasma
-	if (dist(x,y,fx,fy) < raio+tamBloco/2) {
-		if ( trocouCor == false) { 
-			corBranco = ! corBranco;
-			trocouCor = true;
-			vidas--;
+		
+		//relacao do fantasma com fim do espaço
+		if ( fx > 400) {
+			fx = 0;   
 		}
+		if ( fy > 400) {
+			fy = 0;   
+		}
+		if ( fx < 0 ) {
+			fx = 400;   
+		}
+		if ( fy < 0 ) {
+			fy = 400;  
+		}
+		
+		//colisão entre pacMan e fantasma
+		if (dist(x,y,fx,fy) < raio+tamBloco/2) {
+			if ( trocouCor == false) { 
+				corBranco = ! corBranco;
+				trocouCor = true;
+				vidas--;
+			}
+		}
+		else {
+			trocouCor = false;  
+		}
+		
+		if ( corBranco ) {
+			fill(255);
+		}	
+		else {
+			fill(255,0,0);
+		}
+			
+		text("Vidas: "+vidas, 20, 430);
+		text("Pontos: "+pontos, 240, 430);
+		textSize(25);
+		ellipse(fx, fy, 2*raio, 2*raio);
+		pacMan(x, y);
 	}
 	else {
-		trocouCor = false;  
+	text("Game Over", 150, 200);
 	}
-	
-	if ( corBranco ) {
-		fill(255);
-	}	
-	else {
-		fill(255,0,0);
-	}
-	
-/*	for ( i = 0; i < qt; i++) {
-		vx[i] = vx[i] + vdx[i];
-		vy[i] = vy[i] + vdy[i];
-		//bater na parede e volta
-		if ( vy[i] > height || vy[i] < 0 ) {
-			vdy[i] = -vdy[i]; 
-      
-		}
-		if ( vx[i] > width || vx[i] < 0 ) {
-			vdx[i] = -vdx[i]; 
-      
-		}
-    	ellipse(vx[i],vy[i],vtam,vtam); 
-	} */
-
-		
-	text("Vidas: "+vidas, 20, 430);
-	text("Pontos: "+pontos, 240, 430);
-	textSize(25);
-	ellipse(fx, fy, 2*raio, 2*raio);
-	pacMan(x, y);
 }
